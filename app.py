62bd093b4cc4046a0a2917a877de495529e67186
Lifia1 <lfianasywa@gmail.com>
2026-06-03 15:24:59 +0700

    apdet

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def api_cari():
 # RUN
 # ============================================================
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, use_reloader=False)
